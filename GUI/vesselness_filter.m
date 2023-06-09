68d9e1708232ca6be279fd04fa1e4f8a8c99000d
edggjames <33573579+edggjames@users.noreply.github.com>
2023-06-09 10:27:01 +0100

    Add GPL3 licence

--- GUI/vesselness_filter.m
+++ GUI/vesselness_filter.m
@@ -1,3 +1,19 @@
+% This file is part of Vesselness_Filter_Tool.
+% Copyright (C) 2023 Edward James.
+
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Lesser Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+ 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU Lesser Public License for more details.
+   
+% You should have received a copy of the GNU Lesser Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 function If = vesselness_filter(I, sigma, beta)
 % VESSELNESS_FILTER filters a 2D image resulting in the enhancement of
 % line-like features that correspond to blood vessels. 
