e0f61e402fad7bf9c1cc717c0647e3133c562175
edggjames <33573579+edggjames@users.noreply.github.com>
2023-06-09 10:27:55 +0100

    Add GPL3 licence

--- GUI/vesselness_filter_tool.m
+++ GUI/vesselness_filter_tool.m
@@ -1,3 +1,18 @@
+% This file is part of Vesselness_Filter_Tool.
+% Copyright (C) 2023 Edward James.
+
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Lesser Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+ 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU Lesser Public License for more details.
+   
+% You should have received a copy of the GNU Lesser Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
 % Vesselness_Filter_Tool
 % Script to implement GUI for Vessleness_Filter_Tool. Allows user to load
 % an image, select filter parameters, filter the image with a Frangi
